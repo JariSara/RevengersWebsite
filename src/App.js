dd0e8df4c3d51086f50dd5b1616d3146d85e075d
JariSara <57097679+JariSara@users.noreply.github.com>
2023-03-22 02:01:21 +0100

    change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import FrontPage from "./Components/Front/Frontpage.tsx";
+import './index.css';
+
+const BackgroundImagePage = () => {
+  return (
+    <div className="bg"></div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <BackgroundImagePage>gaga</BackgroundImagePage>  
     </div>
   );
 }
---
+++ src/Components/Front/Frontpage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Home(){
+    return(
+        <body>
+            <p>tesssdt</p>
+        </body>
+    )
+}
+export default Home;
