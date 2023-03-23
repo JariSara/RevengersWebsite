cdef441e34c82942fd7752fa89ab049443acd6ee
JariSara <57097679+JariSara@users.noreply.github.com>
2023-03-23 23:19:13 +0100

    small changes

--- src/App.js
+++ src/App.js
@@ -2,28 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import FrontPage from "./Components/Front/Frontpage.tsx";
 import './index.css';
-
-const BackgroundImagePage = () => {
-  return (
-    <div className="bg"></div>
-  );
-}
-
-
 import React, { Component } from 'react';
-  
+
 class App extends Component {
   render() {
     const myStyle={
-backgroundImage:`url(${process.env.PUBLIC_URL+ "/val.jpg"})`,
+        backgroundImage:`url(${process.env.PUBLIC_URL+ "/val.jpg"})`,
         height:'100vh',
         marginTop:'-70px',
         fontSize:'50px',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
+        position: "relative"
         };
     return (
-      <div style={myStyle} >
+
+      <div style={myStyle}>
+        <FrontPage></FrontPage>
       </div>
     );
   }
