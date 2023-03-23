40583f008ac763a540b31f2da6e503d97bd0bf54
JariSara <57097679+JariSara@users.noreply.github.com>
2023-03-23 23:07:01 +0100

    background image

--- src/App.js
+++ src/App.js
@@ -9,12 +9,24 @@ const BackgroundImagePage = () => {
   );
 }
 
-function App() {
-  return (
-    <div>
-    <BackgroundImagePage>gaga</BackgroundImagePage> 
-    </div>
-  );
+
+import React, { Component } from 'react';
+  
+class App extends Component {
+  render() {
+    const myStyle={
+backgroundImage:`url(${process.env.PUBLIC_URL+ "/val.jpg"})`,
+        height:'100vh',
+        marginTop:'-70px',
+        fontSize:'50px',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        };
+    return (
+      <div style={myStyle} >
+      </div>
+    );
+  }
 }
 
 export default App;
