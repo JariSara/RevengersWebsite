24740a9661e0390e4627a016b86a608164ea5792
JariSara <57097679+JariSara@users.noreply.github.com>
2023-10-02 03:23:11 +0200

    some final edits
    still missing the responsive version

--- src/Components/PlayerInfo/Players.tsx
+++ src/Components/PlayerInfo/Players.tsx
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
 import Card from '@mui/material/Card';
-
-import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 
 import Typography from '@mui/material/Typography';
 import '../../index.css';
 import './Players.css';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
-import { Box, CardActions, Collapse, Grid } from '@mui/material';
+import { Box, Button, CardActions, Collapse, Grid, Modal } from '@mui/material';
 import { styled } from '@mui/material/styles';
 //import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
 }
-
+const style2 = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  transparent: true,
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 const myStyle = {
   backgroundImage: `url(${process.env.PUBLIC_URL + "/val.jpg"})`,
   height: '100vh',
@@ -50,46 +58,77 @@ const CardInfo = function (playerName, playerInformation, imageName, typeOfPlaye
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+  const [mouseOver, setMouseOver] = useState(false);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
 
     <div className="divAdjuster">
-      <Card sx={{
-        maxWidth: 500, ':hover': {
-          boxShadow: 20
-        }
-      }} style={{ backgroundColor: "transparent" }}>
-        <div onMouseEnter={e => {
-          setStyle({ display: 'block' });
-        }}
-          onMouseLeave={e => {
-            setStyle({ display: 'none' })
-          }}>
+      <Button onClick={handleOpen}>
+        <Card sx={{
+          maxWidth: 500
+        }} style={{ backgroundColor: "transparent" }}>
+          <div onMouseEnter={e => {
+            setMouseOver(true);
+          }}
+            onMouseLeave={e => {
+              setMouseOver(false)
+            }}>
 
-          <div >
-            <Box sx={{ border: 85, borderColor: 'transparent' }}>
-              <div>
-                <CardMedia
-                  component="img"
-                  alt="green iguana"
-                  height="140"
-                  image={imageName}
-                />
+            <div >
+              <Box sx={{ border: 50, borderColor: 'transparent', width: "200px", height: "280px" }}>
+                <div>
+                  <CardMedia
+                    component="img"
+                    alt="green iguana"
+                    height="140"
+                    image={imageName}
+                  />
+                  <p className="newFontt">{playerN}</p>
+                  {mouseOver ? (<><p className="BoxedText">{typeOfPlayer}</p><p className="newFontt">{ageAndCountry}</p> </>) : (<p></p>)}
+                  <p className="BoxedText" style={style}>{typeOfPlayer}</p>
+                  <p className="newFontt" style={style}>{ageAndCountry}</p>
 
-                <p className="newFontt">{playerN}</p>
-                <p className="BoxedText">{typeOfPlayer}</p>
-                <p className="newFontt">{ageAndCountry}</p>
-                <p className='newFontt' style={style}>{playerI}</p>
-              </div>
-            </Box>
+                </div>
+              </Box>
+            </div>
+
           </div>
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
+        </Card>
 
-            </Typography>
-          </CardContent>
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <div className="modalAdjuster">
+          <div>
 
+            <Card sx={{
+              maxWidth: 600
+            }} style={{ backgroundColor: "transparent" }}>
+              <div>
+                <Box sx={{ border: 50, borderColor: 'transparent', width: "400px", height: "840px" }}>
+                  <div>
+                    <CardMedia
+                      component="img"
+                      alt="green iguana"
+                      height="280"
+
+                      image={imageName}
+                    />
+                    <p className="newFontt">{playerN}</p>
+                    <p className="BoxedText">{typeOfPlayer}</p>
+                    <p className="newFontt">{ageAndCountry}</p>
+                    <p className="newFontt">{playerInformation}</p>
+                  </div>
+                </Box>
+              </div>
+            </Card>
+          </div>
         </div>
-      </Card>
+      </Modal>
     </div>
 
   )
@@ -98,14 +137,16 @@ const CardInfo = function (playerName, playerInformation, imageName, typeOfPlaye
 
 function Players() {
 
-  const rea = CardInfo("Rea", "Rea is one of our most unique players. With a wide range of agents in his arsenal, this child manages to rescue the team every match. His aggressive style makes the opponents slam their keyboard in rage. Although his teammates are not happy about the aggressive peeks, the results speak for themselves. ", "../sky.gif", "player", "24, Norway");
+  const rea = CardInfo("Rea", "Rea is one of our most unique players. With a wide range of agents in his arsenal, this child manages to rescue the team every match. His aggressive style makes the opponents slam their keyboard in rage. Although his teammates are not happy about the aggressive peeks, the results speak for themselves. ", "../sky.gif", "player", "21, Norway");
   const DTZomby = CardInfo("DTZomby", "Carlos har kjempestor kuk", "../omen.gif", "player", "27, Norway");
-  const flame = CardInfo("Flame", "Flame har banket somalere", "../kayo.gif", "player", "24, Latvia");
+  const flame = CardInfo("Flame", "Flame har banket somalere", "../kj.gif", "player", "24, Latvia");
   const brotherhanky = CardInfo("hanky", "Brother linups", "../viper.gif", "player", "27, Norway");
   const toctic = CardInfo("TocTic", "Toctic er mastermind", "../breach.gif", "player", "27, Norway");
+  const Mange = CardInfo("Mange", "Radiant chad", "../cypher.gif", "stand in", "24, Sweden");
+  const Dinkelbergir = CardInfo("Dinkel", "topp chad", "../brim.gif", "coach", "28, Norway");
+  const Kraize = CardInfo("Stortiltarn", "fucking faggot", "../sova.gif", "stand in", "28, Norway");
   return (
     <div>
-      <p>Hello</p>
       <div className='flexing'>
 
         {DTZomby}
@@ -117,7 +158,13 @@ function Players() {
         {brotherhanky}
 
         {toctic}
+      </div>
+      <div className='flexing'>
+        {Mange}
 
+        {Dinkelbergir}
+
+        {Kraize}
       </div>
     </div>
   )
